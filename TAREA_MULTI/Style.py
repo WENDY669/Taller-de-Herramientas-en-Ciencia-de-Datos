8f1e78a2cc9f41a9900c32ffa65f65f288e35a00
WENDY669 <220300772@ucaribe.edu.mx>
2025-01-23 12:30:50 -0500

    Add files via upload

---
+++ TAREA_MULTI/Style.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.markdown(
+    """
+    <style>
+    div.stButton > button{
+        
+        color:white;
+        margin: 0 10px;
+        padding: 10px 20px;
+        border-radius: 5px;
+        }
+</style>
+""",
+unsafe_allow_html=True
+
+)
