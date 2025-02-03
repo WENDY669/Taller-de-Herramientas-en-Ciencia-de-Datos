73324618111c8fd2d92e6d0fd8a69c193481bdbe
WENDY669 <220300772@ucaribe.edu.mx>
2025-02-02 19:49:56 -0500

    Update Introduccion.py

--- TAREA_OCDE/Introduccion.py
+++ TAREA_OCDE/Introduccion.py
@@ -1,20 +1,61 @@
 import streamlit as st
-import pandas as pd
 
-df = pd.read_csv('datos/OECD.csv')
+st.title("Esperanza de vida de los países de la OCDE")
 
-def show():
-    st.title("Introducción a los Datos de Esperanza de Vida")
-    st.write(f"""
-    Este conjunto de datos contiene información sobre la esperanza de vida en países pertenecientes y no pertenecientes a la OCDE.
-    La fuente de los datos proviene de la Organización para la Cooperación y el Desarrollo Económicos (OCDE), abarcando desde el 2015 hasta el 2023, proporcionando una visión global de la evolución de la esperanza de vida en todos los países listados.
+st.subheader("Origen")
+st.markdown("""
+            El dataset proviene de la Organización para la Cooperación y el Desarrollo Económico (OCDE), 
+            lo que indica que los datos son confiables y han sido recopilados con fines estadísticos 
+            y de investigación sobre salud pública y calidad de vida.
+            """)
 
-    Los datos están organizados por país y año, y incluyen las  categorías:
-    - **Esperanza de vida total**: Promedio hombres y mujeres.
-    - **Esperanza de vida para hombres**: Datos específicos para el género masculino.
-    - **Esperanza de vida para mujeres**: Datos específicos para el género femenino.
 
-    En cuanto a los países, la mayoria están clasificados como miembros de la OCDE y una minoria no. Los países miembros de la OCDE están identificados con el valor "OCDE" en la columna correspondiente, mientras que los países no miembros tienen el valor "No OCDE".
+st.subheader("Objetivo")
+st.markdown("""
+            El principal propósito de estos datos es proporcionar información sobre la esperanza de vida 
+            en distintos países a lo largo de los años, diferenciando entre géneros y pertenencia a la OCDE.
+            """)
 
-    La información de este conjunto de datos abarca un total de **{df['Pais'].nunique()} países**.
-    """)
+st.subheader("Indicadores")
+st.markdown("""
+            * OCDE: Indica si el país pertenece a la Organización para la Cooperación y el Desarrollo Económicos (OCDE) (Sí o No).
+            * Codigo_Pais: Código del país en formato abreviado.
+            * Pais: Nombre del país.
+            * Año: Año al que corresponden los datos.
+            * Female: Esperanza de vida en años para mujeres.
+            * Male: Esperanza de vida en años para hombres.
+            * Total: Esperanza de vida promedio en el país.
+            """)
+
+
+st.subheader("Características")
+st.markdown("""
+            El conjunto de datos "Life_expectancy" contiene información
+            sobre la esperanza de vida en distintos países de la OCDE.
+
+            Este dataset permite analizar cómo varía la esperanza de vida 
+            según distintos factores; con datos anuales de esperanza de vida 
+            por país, con diferenciación por género y pertenencia a la OCDE.
+
+            Algunas de las características principales de nuestra planta 
+            de datos son:
+
+            * Cobertura geográfica: Diferentes países identificados por códigos y nombres
+            * Frecuencia de datos: Anual
+            * Unidad de medida: Años de esperanza de vida
+            * Fuente: Datos extraídos de la OCDE, lo que garantiza su validez y confiabilidad
+
+            El dataset contiene datos de 49 países en total:
+            * 37 países pertenecen a la OCDE.
+            * 12 países no pertenecen a la OCDE.
+            """)
+
+
+st.subheader("Agrupación por género")
+st.markdown("""
+            Los datos se dividen en tres categorías:
+
+            * Female: Esperanza de vida de mujeres.
+            * Male: Esperanza de vida de hombres.
+            * Total: Promedio general de la esperanza de vida en cada país.
+             """)
